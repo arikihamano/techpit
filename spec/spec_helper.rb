f0f01f367f0bc0ca56ef722a13affeb213a092bd
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-05-26 08:27:48 +0900

    finish6-2 and fixed 6-1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ require 'capybara/rspec'
 RSpec.configure do |config|
 
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome, screen_size: { 1280, 960 }
+    driven_by :selenium, using: :chrome, screen_size: [ 1280, 960 ]
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
